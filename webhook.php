04ec94407894e3b53760b8c64d1e7b6627654d0e
peem-kantapat <93250095+peem-kantapat@users.noreply.github.com>
2021-10-27 15:56:14 +0700

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -1,6 +1,6 @@
 <?php
 
-$channelAccessToken = 'PZ6qlbYABvcIg+sly4KFcjs8rAVOW1+EEEDBgcOn86a9MwA+MNHV8//FPERaqcVuWnKEs4U+6oe0jLA++fQlGKdK9/SCRKlZ0x4otRbscQZBRbe5VDkXvu32iZAA+dpXEwrb47Ncr9kuH1vSp+t3LwdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$channelAccessToken = 'Bm8ZHgYq+KobICVm/Gk4OtShM+F+2QDCWG5sD720DQp5cWGneQJcAv8hZVT1JC0mZaWdoL3JZDdepLBpJHIwCwdTW6dH7y9rerlY6KxYNDh5d0qzap0nYSlezd3h7nOaCGw2CQ9laqXppA+jVBZ9IAdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
 
 $request = file_get_contents('php://input');   // Get request content
 
